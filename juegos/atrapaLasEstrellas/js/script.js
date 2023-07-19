cb057c0cf176e0dcf522e03f509c584ab2806bba
DVM09 <dylanvm09@gmail.com>
2023-07-18 22:36:03 -0600

    AVANCES ESTRELLAS - JUEGUITO TICS

--- juegos/atrapaLasEstrellas/js/script.js
+++ juegos/atrapaLasEstrellas/js/script.js
@@ -1,8 +1,8 @@
-const pj= document.getElementById("personaje");
-let seconds;
+const pj = document.getElementById("personaje");
+let seconds; 
+let estrellasTotales = 0;
 document.addEventListener("DOMContentLoaded", function(event){
-    const pisos = Array.from(document.getElementsByClassName("piso"));
-    let posicion = { x: 430, y: 200};
+    let posicion = { x: 430, y: 300};
     function handleKeyPress(event) {
         let newX = posicion.x;
         let newY = posicion.y;
@@ -36,16 +36,11 @@ document.addEventListener("DOMContentLoaded", function(event){
         }
     }
     function MovimientoValido(x, y) {
-        if (x < 0 || x > 1300 || y < 0 || y > 500) {
+        if (x < 0 || x > 1130 ) 
             return false;
-        }
-        for (let piso of pisos) {
-            if (x === piso.offsetLeft && y === piso.offsetTop) {
-                return false;
-            }
-        }
         return true;
     }   
+    
     window.addEventListener("keydown", handleKeyPress);
 });
 const JuegoPerdido = () =>{
@@ -55,25 +50,55 @@ let tiempo=75;
 let juegoEmpezado = false;
 let tiempoSetInterval;
 let crono = document.getElementById('cronometro');
+const listaEstrellas = Array.from(document.querySelector('.estrella'));
+let estrellaBajando = 0;
+
 const ConvertirTiempo = (num) => {
    const minutos =  Math.floor(num / 60);
-   let segundos = num % 60;
+   let segundos = num % 60; 
    if(segundos<10)
        segundos = `0${segundos}`
    crono.innerHTML = `${minutos}:${segundos}`
 }
-const ActuTiempo = () => {
-    if(tiempo<=0){
-        JuegoPerdido();
-    }
+
+const NumeroRandom = () => {
+    let numero = Math.floor(Math.random() * 1250)
+    return numero
+}
+
+const PosicionEstrellas = () => {
+    for(let estrella in listaEstrellas)
+        estrella.style.left = `${NumeroRandom()}px`
+}
+const EstrellaNueva = () => {
+    listaEstrellas[estrellaBajando].style.display = 'block';
+    if(estrellaBajando === 9)
+        estrellaBajando = 0;
     else
-    {
+        estrellaBajando++;
+}
+const ActuTiempo = () => {
+    if(tiempo<=0)
+        JuegoPerdido();
+    else
         tiempo--;
-    }
     ConvertirTiempo(tiempo);
+}
+const DetectarColision = () => {
+    
+    ActuContador();
+}
+const ActuContador = () => {
+    const contador = document.getElementById('contadorEstrellas');
+    contador.innerHTML = estrellasTotales;
 }
 const IniciarJuego = () => {
     const ePJ = document.getElementById('iniciar');
     ePJ.style.display = "none";
     setInterval(ActuTiempo,1000);
+    PosicionEstrellas();
+    EstrellaNueva();
+    setInterval(EstrellaNueva,5000);
+    setInterval(PosicionEstrellas,40000);
+    setInterval(DetectarColision,100)
 }

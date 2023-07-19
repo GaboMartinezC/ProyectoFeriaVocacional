2ab7e92bb01b7c4a7d3bb96afc63da0569081b50
GaboMartinezC <martinezcgabriel24@gmail.com>
2023-07-18 22:20:37 -0600

    navegacion corregido

--- js/capturas.js
+++ js/capturas.js
@@ -1,13 +1,3 @@
-let variableControl = false;
-const lstOpciones = document.getElementsByClassName('opcionesMenuJuegos');
-for (let i = 0; i < lstOpciones.length; i++){
-    lstOpciones[i].addEventListener('onmouseleave', function ImagenDefecto(){
-        if (!variableControl)
-        {
-            console.log("sexooo")
-        }
-    });
-} 
 const CambiarCaptura = (juego) => {
     let opcion = "assets/img/" + juego + ".png";
     let opcionJuego = "juegos/" + juego + "/index.html";
